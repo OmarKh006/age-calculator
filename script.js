fece62c35588d35a7b1a3ad705fd5586dcfda909
OmarKh006 <khairyomar006@gmail.com>
2026-04-10 15:11:00 +0200

    input validation and finishing the project

--- script.js
+++ script.js
@@ -6,6 +6,51 @@ const resultValue = document.querySelector(".card__resultValue");
 const calcBtn = document.querySelector(".card__button");
 const today = new Date();
 
+const validateDay = (day) => {
+  if (day && day > 0 && day <= 31) {
+    return true;
+  }
+};
+
+const validateMonth = (month) => {
+  if (month && month > 0 && month <= 12) {
+    return true;
+  }
+};
+
+const validateYear = (year) => {
+  if (year && year > 0 && year <= today.getFullYear()) {
+    return true;
+  }
+};
+
+const validateDate = (dayInput, monthInput, yearInput) => {
+  let validDate = [false, false, false];
+
+  if (!validateDay(dayInput.value)) {
+    dayInput.classList.add("card__input--error");
+  } else {
+    validDate[0] = true;
+    dayInput.classList.remove("card__input--error");
+  }
+
+  if (!validateMonth(monthInput.value)) {
+    monthInput.classList.add("card__input--error");
+  } else {
+    validDate[1] = true;
+    monthInput.classList.remove("card__input--error");
+  }
+
+  if (!validateYear(yearInput.value)) {
+    yearInput.classList.add("card__input--error");
+  } else {
+    validDate[2] = true;
+    yearInput.classList.remove("card__input--error");
+  }
+
+  return validDate.every((item) => item === true);
+};
+
 const calculateAge = (day, month, year) => {
   const birthDate = new Date(year, month - 1, day);
   let age = today.getFullYear() - birthDate.getFullYear();
@@ -19,6 +64,12 @@ const calculateAge = (day, month, year) => {
 
 const onclickHandler = () => {
   const age = calculateAge(inputDay.value, inputMonth.value, inputYear.value);
+
+  if (!validateDate(inputDay, inputMonth, inputYear)) {
+    resultValue.textContent == "--";
+    return;
+  }
+
   resultValue.innerHTML = age;
 };
 
